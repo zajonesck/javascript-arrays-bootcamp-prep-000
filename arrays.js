8a325a43ffb8d99e3258935f520d50b44ed12a3c
Zachary Clinton Jones <zajonesck83@gmail.com>
2018-02-12 18:13:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,4 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-//var candyString = "foo"
  
 function addElementToBeginningOfArray(chocolateBars, candyString){
   return [candyString, ...chocolateBars];
